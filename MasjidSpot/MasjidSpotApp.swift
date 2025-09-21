ba6d7b56beb2bdfd19ccfc938b5a3d5b85ecedb5
tambatech <aka_lamin@hotmail.com>
2025-09-21 20:44:58 +0200

    Initial Commit

---
+++ MasjidSpot/MasjidSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasjidSpotApp.swift
+//  MasjidSpot
+//
+//  Created by Lamin Tamba on 9/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MasjidSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
